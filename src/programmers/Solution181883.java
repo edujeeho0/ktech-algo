9b4c293f7a1d4f5cef7ca4b424e9db0e2f3ad49f
edujeeho <edujeeho@gmail.com>
2024-07-22 08:47:46 +0900

    programmers

--- src/programmers/Solution181883.java
+++ src/programmers/Solution181883.java
@@ -5,12 +5,18 @@ public class Solution181883 {
     public int[] solution(int[] arr, int[][] queries) {
         int[] answer = arr;
         // queries의 갯수만큼 반복한다.
-            // int[] query = queries[i];
+        for (int i = 0; i < queries.length; i++) {
+            int[] query = queries[i];
             // start는 query[0]
             // end는 query[1]
+            int start = query[0];
+            int end = query[1];
             // start 부터 end까지 answer의 배열을
+            for (int j = start; j < end + 1; j++) {
                 // += 1 한다.
-
+                answer[j]++;
+            }
+        }
         return answer;
     }
 }
--- src/programmers/Solution181932.java
+++ src/programmers/Solution181932.java
@@ -1,15 +1,24 @@
 package programmers;
 
-// https://school.programmers.co.kr/learn/courses/30/lessons/181883
+// https://school.programmers.co.kr/learn/courses/30/lessons/181932
 public class Solution181932 {
     public String solution(String code) {
-        StringBuilder answer = new StringBuilder();
+        StringBuilder answerBuilder = new StringBuilder();
         // mode를 기록한 다음
+        int mode = '0';
         // code의 각 글자를 확인해가면서
+        for(int i = 0; i < code.length(); i++) {
+            char letter = code.charAt(i);
+            // 글자가 1이면 mode 변경
+            if (letter == '1') mode = '1' - mode + '0';
             // mode가 0일때는 짝수번째 글자들을
             // mode가 1일때는 홀수번째 글자들을
             // answer에다가 추가해준다.
-
+            else if (i % 2 == mode - '0') {
+                answerBuilder.append(letter);
+            }
+        }
+        String answer = answerBuilder.toString();
         return answer.isEmpty() ? "EMPTY" : answer.toString();
     }
 }
