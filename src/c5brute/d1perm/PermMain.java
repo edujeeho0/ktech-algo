361122864f6fa503e9e6990204f0ceb03f77aa80
edujeeho <edujeeho@gmail.com>
2024-07-25 13:41:38 +0900

    permutation by recursion

---
+++ src/c5brute/d1perm/PermMain.java
@@ -0,0 +1,8 @@
+package c5brute.d1perm;
+
+public class PermMain {
+    public static void main(String[] args) {
+        Permutation permutation = new Permutation();
+        permutation.permutation(20, 10);
+    }
+}
--- src/c5brute/d1perm/Permutation.java
+++ src/c5brute/d1perm/Permutation.java
@@ -1,5 +1,7 @@
 package c5brute.d1perm;
 
+import java.util.Arrays;
+
 public class Permutation {
     public static void main(String[] args) {
         int[] data = { 4, 5, 6 };
@@ -49,6 +51,46 @@ public class Permutation {
                     System.out.println(i + " " + j + " " + k);
                 }
             }
+        }
+    }
+
+    // nPr을 구하는 메서드
+    // 0 부터 n - 1 사이의 숫자 중
+    // r개를 골라서 나열하는 메서드
+    public void permutation(
+            // nPr
+            int n, int r) {
+        int[] perm = new int[r];
+        boolean[] used = new boolean[n];
+        permRecursive(n, r, 0, perm, used);
+    }
+
+    private void permRecursive(
+            int n, int r,
+            // 몇번째 원소를 골랐는지 표시하는 변수
+            int k,
+            // 현재 만들고 있는 순열을 저장할 변수
+            int[] perm,
+            // 사용한 숫자를 판단할 배열
+            boolean[] used
+    ) {
+        if (k == r) {
+            System.out.println(Arrays.toString(perm));
+            return;
+        }
+        // 숫자를 0 ~ n-1 까지 살펴본다.
+        for (int i = 0; i < n; i++) {
+            // i가 이미 사용되었으면 건너뛴다.
+            if (used[i]) continue;
+            // 이번에 선택한 결과(i)를
+            // 내가 만들고 있는 순열의 k에 추가한다.
+            perm[k] = i;
+            // 사용했다고 기록
+            used[i] = true;
+            // 다음 숫자를 고르라고 재귀호출한다.
+            permRecursive(n, r, k + 1, perm, used);
+            // 재귀호출이 끝나면, 사용했던 숫자를 반납한다.
+            used[i] = false;
         }
     }
 }
